a3364205d52a6197aeea3e1ffac126c4d0fffda9
EveraldoDiniz30 <everaldojr336@gmail.com>
2026-03-12 20:37:00 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,10 @@ import random
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
-TOKEN = "8681180706:AAFsLuGC7uEgazESRF0BMzCVGXZt4boQVss"
+TOKEN = "SEU_TOKEN"
 CHAT_ID = "@siinaismilionarios"
+
+LINK = "https://bit.ly/47BFiyX"
 
 def enviar(msg):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
@@ -36,7 +38,7 @@ while True:
 
     # mensagem 1
     msg1 = f"""
-🚨 NOVO SINAL DETECTADO
+🚨 SINAL DETECTADO
 
 🎮 Jogo: {jogo}
 
@@ -48,7 +50,6 @@ Prepare-se 👀
 
     time.sleep(random.randint(10,20))
 
-    # dados aleatórios do sinal
     jogadas_normal = random.randint(1,20)
     jogadas_turbo = random.randint(1,20)
 
@@ -70,14 +71,15 @@ NOVA ENTRADA ✅
 
 🎰 Sequência: {sequencia}
 
-SÓ FUNCIONA AQUI, APOSTE AGORA 👇🏻👇🏻👇🏻
+SÓ FUNCIONA AQUI 👇🏻👇🏻👇🏻
+{LINK}
 """
 
     enviar(msg2)
 
     time.sleep(random.randint(60,90))
 
-    # resultado 90% win
+    # 90% win
     resultado = "WIN" if random.random() < 0.9 else "LOSS"
 
     if resultado == "WIN":
@@ -86,9 +88,10 @@ SÓ FUNCIONA AQUI, APOSTE AGORA 👇🏻👇🏻👇🏻
 
 {jogo}
 
-Mais um lucro garantido.
+Lucro garantido novamente 🔥
 
-Fique atento ao próximo sinal 🔥
+Aposte aqui 👇🏻
+{LINK}
 """
     else:
         msg3 = f"""
@@ -98,12 +101,14 @@ Fique atento ao próximo sinal 🔥
 
 Mercado variou dessa vez.
 
-Seguimos para o próximo sinal.
+Recupere no próximo sinal 👇🏻
+{LINK}
 """
 
     enviar(msg3)
 
-    tempo = random.randint(120,600)
+    # intervalo 8–25 minutos
+    tempo = random.randint(480,1500)
 
     print("Próximo sinal em:", tempo)
 
